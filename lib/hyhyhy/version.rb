1ae04f6367dab6bbd6e53b762872413f7ccf4c2d
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2015-01-15 19:45:19 +0100

    Change version from 0.0.9 to 1.0.0

--- lib/hyhyhy/version.rb
+++ lib/hyhyhy/version.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
+
 module Hyhyhy
-  VERSION = "0.0.9"
+  VERSION = "1.0.0"
   SUMMARY = %q{Pure & Professional presentations}
   DESCRIPTION = %q{A tool for creating nice looking HTML5 presentations.}
 end
