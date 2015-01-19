0c478e60cc41c7700f920af8bd4ac6d3c26f9177
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2015-01-19 21:27:52 +0100

    Clear all old files!
    The new version will have a new design and structure.

--- lib/hyhyhy/version.rb
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-module Hyhyhy
-  VERSION = "1.0.0"
-  SUMMARY = %q{Pure & Professional presentations}
-  DESCRIPTION = %q{A tool for creating nice looking HTML5 presentations.}
-end
